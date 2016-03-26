785a5a6614b6edaf762f17667aa6205aeaabbe6b
njir <njir@naver.com>
2016-03-26 16:17:42 +0900

    Add react-redux-table-example

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-# redux_practice
-For study
+# React Redux Table Example
 
-ref : https://dobbit.github.io/redux/docs_kr/basics/Actions.html
+[![Build Status](https://travis-ci.org/alyssaq/react-redux-table-example.png?branch=master)](https://travis-ci.org/alyssaq/react-redux-table-example)
+
+Demo: <http://react-redux-table.surge.sh>
+
+Features:
+
+ * [React](https://facebook.github.io/react)-[Redux](http://redux.js.org) data flow
+ * Fetch JSON from an API and render into a table
+ * Filter and sort data in the table
+ * Routing with [react-router](https://github.com/rackt/react-router)
+ * Redux middlewares
+ * Separate reducers and actions
+ * ES6/ES7 with [babeljs](https://babeljs.io) (es2015, stage-0, react)
+ * [Stylus](http://learnboost.github.io/stylus)
+ * [Webpack](https://webpack.github.io) dev and production
+ * Eslint [standard](http://standardjs.com)
+ * Unit tests with [mocha](https://mochajs.org) + [chai](http://chaijs.com)
+
+## Install
+
+    $ npm install
+
+## Run - Development
+
+    $ npm run dev       # builds and hot reloads on changes
+
+## Run - Production
+
+    $ npm run build     # builds production assets (transpile, minify, etc)
+    $ npm start         # Start express server and serves index.html
+
+## Tests
+
+    $ npm run lint      # Runs eslint
+    $ npm test          # Runs mocha
+    $ npm run test:dev  # Run mocha in watch mode
+
+## Thanks
+
+Data from [USDA nutrient API](http://ndb.nal.usda.gov/ndb/doc/apilist/API-NUTRIENT-REPORT.md)
---
+++ src/components/NutrientTable/NutrientTable.js
@@ -0,0 +1,138 @@
+import React from 'react'
+import { Column, Cell } from 'fixed-data-table'
+import ResponsiveTableWrapper from '../ResponsiveTableWrapper'
+import renderers from '../../modules/renderers'
+
+// Stateless cell components for Table component
+function SortHeaderCell ({children, columnKey, ...props}) {
+  const clickFunc = () => props.sortBy(columnKey)
+
+  return (
+    <Cell {...props}>
+      <a onClick={clickFunc}>
+        {children} {renderers.renderSortArrow(props, columnKey)}
+      </a>
+    </Cell>
+  )
+}
+
+SortHeaderCell.propTypes = {
+  sortBy: React.PropTypes.func.isRequired,
+  columnKey: React.PropTypes.string,
+  children: React.PropTypes.any
+}
+
+function DataCell ({data, rowIndex, columnKey, ...props}) {
+  return <Cell {...props}> {data[rowIndex][columnKey]} </Cell>
+}
+
+DataCell.propTypes = {
+  data: React.PropTypes.array,
+  rowIndex: React.PropTypes.number,
+  columnKey: React.PropTypes.string
+}
+
+class NutrientTable extends React.Component {
+  componentWillMount () {
+    this.props.fetchData()
+  }
+
+  handleFilterStringChange (e) {
+    e.preventDefault()
+    this.props.filterBy(e.target.value)
+  }
+
+  handleSortClick (label, key) {
+    const sortFunc = () => this.props.sortBy(key)
+    return <a onClick={sortFunc}>{label}</a>
+  }
+
+  doesMatch (str) {
+    return (key) => (key + '').toLowerCase().indexOf(str) !== -1
+  }
+
+  filterData () {
+    const {data, filterString} = this.props
+    const str = filterString.toLowerCase()
+    return str !== ''
+      ? data.filter((r) => Object.values(r).some(this.doesMatch(str)))
+      : data
+  }
+
+  sortData () {
+    const {data, sortKey, sortDesc} = this.props
+    const multiplier = sortDesc ? -1 : 1
+    data.sort((a, b) => {
+      const aVal = a[sortKey] || 0
+      const bVal = b[sortKey] || 0
+      return aVal > bVal ? multiplier : (aVal < bVal ? -multiplier : 0)
+    })
+    return this
+  }
+
+  render () {
+    const { isFetching, filterString } = this.props
+    const data = this.sortData().filterData()
+
+    return (
+      <div>
+        <input className='filter-input' value={filterString}
+          onChange={::this.handleFilterStringChange}
+          type='text' placeholder='Filter Rows'
+          autoCorrect='off' autoCapitalize='off' spellCheck='false'/>
+        <br/>
+
+        {isFetching && data.length === 0 &&
+          <div className='loader-box'></div>}
+        {!isFetching && data.length === 0 &&
+          <h3 className='center'>No Matching Results :( </h3>}
+
+        <ResponsiveTableWrapper
+          rowHeight={50}
+          headerHeight={50}
+          rowsCount={data.length}>
+          <Column
+            columnKey='food'
+            header={<SortHeaderCell {...this.props}> Food </SortHeaderCell>}
+            cell={<DataCell data={data}/>}
+            flexGrow={3}
+            width={100} />
+          <Column
+            columnKey='nutrient'
+            header={<SortHeaderCell {...this.props}> Nutrient </SortHeaderCell>}
+            cell={<DataCell data={data}/>}
+            flexGrow={1}
+            width={100} />
+          <Column
+            columnKey='value'
+            header={<SortHeaderCell {...this.props}> Value </SortHeaderCell>}
+            cell={<DataCell data={data}/>}
+            flexGrow={0.5}
+            width={100} />
+          <Column
+            columnKey='unit'
+            header={<SortHeaderCell {...this.props}> Unit </SortHeaderCell>}
+            cell={<DataCell data={data}/>}
+            flexGrow={0.1}
+            width={100} />
+        </ResponsiveTableWrapper>
+      </div>
+    )
+  }
+}
+
+NutrientTable.propTypes = {
+  // actions
+  fetchData: React.PropTypes.func.isRequired,
+  sortBy: React.PropTypes.func.isRequired,
+  filterBy: React.PropTypes.func.isRequired,
+
+  // state data
+  data: React.PropTypes.array.isRequired,
+  filterString: React.PropTypes.string.isRequired,
+  sortKey: React.PropTypes.string.isRequired,
+  sortDesc: React.PropTypes.bool.isRequired,
+  isFetching: React.PropTypes.bool.isRequired
+}
+
+export default NutrientTable
